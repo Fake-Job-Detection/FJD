ae8902800f733164f2d7b23f6f9f598f9d8ce6a9
AbdulQuadir101 <61408593+AbdulQuadir101@users.noreply.github.com>
2021-02-20 22:45:25 +0530

    While loop

--- ProperOne.java
+++ ProperOne.java
@@ -1,5 +1,8 @@
 public class ProperOne{
     public static void main(String args[]){
             System.out.println("Pandi git podaaaang");
+            While(true){
+                System.out.println("kill git");
+            }
     }
 }
