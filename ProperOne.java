cf8935aa5a01db8dd97b2c0670be16051bf23cca
M.R.M.Omar <omar.2018527@iit.ac.lk>
2021-02-21 19:18:18 +0530

    Removed file

--- ProperOne.java
+++
@@ -1,9 +0,0 @@
-public class ProperOne{
-    public static void main(String args[]){
-            System.out.println("Pandi git podaaaang");
-            While(true){
-                System.out.println("Convert Java to Python , code yakoooooooooo");
-        
-            }
-    }
-}
