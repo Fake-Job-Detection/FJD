36313a3dcb68f85a9b06d759c4561bb3062ebdc2
M.R.M.Omar <omar.2018527@iit.ac.lk>
2021-04-20 20:18:52 +0530

    Prediction output function updated

--- DataScience/Predict.py
+++ DataScience/Predict.py
@@ -1,5 +1,11 @@
 import pickle
 from Exceptionhandling import handle
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from Dataread import read_csv
 
 
 def load_model_predict(data):
@@ -17,3 +23,28 @@ def load_model_predict(data):
         score_and_save(y_pred)
     except Exception as e:
         handle('prediction process')
+
+
+def score_and_save(y_pred):
+    try:
+        data = read_csv('Data/test.csv')
+
+        y_test = data['fraudulent']
+        cm = confusion_matrix(y_test, y_pred)
+        print("\n" + "SCORES")
+        print("confusion matrix")
+        print(cm)
+        print('F1-Score' + ' = ' + str(round(f1_score(y_test, y_pred), 4)))
+        print('Precision' + ' = ' + str(round(precision_score(y_test, y_pred), 4)))
+        print('Recall' + ' = ' + str(round(recall_score(y_test, y_pred), 4)))
+        print('Accuracy' + ' = ' + str(round(accuracy_score(y_test, y_pred), 4)))
+
+        data['fraud_prediction'] = y_pred
+
+        data.to_csv('predictionoutput/testsetprediction.csv')
+
+        # newData = read_csv('predictionoutput/testsetprediction.csv')
+        # print(newData.head(20))
+
+    except Exception as e:
+        handle('scoring and saving process')
