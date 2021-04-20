1674def58f078633ce12e9770b80c9691f7ade31
M.R.M.Omar <omar.2018527@iit.ac.lk>
2021-04-20 17:03:55 +0530

    Prediction model loading testing updated

--- DataScience/testing.py
+++ DataScience/testing.py
@@ -1,14 +1,14 @@
-from Allcodefiles.Dataread import read_csv
-from Allcodefiles.DataHandling import missing_values
-from Allcodefiles.DataHandling import texthandling
-from Allcodefiles.DataHandling import categorical_cols_test
-from Allcodefiles.Predict import load_model_predict
-from Allcodefiles.Exceptionhandling import handle
+from Dataread import read_csv
+from DataHandling import missing_values
+from DataHandling import texthandling
+from DataHandling import categorical_cols_test
+from Predict import load_model_predict
+from Exceptionhandling import handle
 
 
 def testing():
     try:
-        data = read_csv('data/test.csv')
+        data = read_csv('Data/test.csv')
 
         (data.pipe(missing_values).pipe(texthandling)
              .pipe(categorical_cols_test).pipe(load_model_predict))
