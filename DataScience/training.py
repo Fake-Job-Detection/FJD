1f819c01847005dcf55930d4d8ca241355707945
M.R.M.Omar <omar.2018527@iit.ac.lk>
2021-04-20 16:55:21 +0530

    Referrencing file error sorted

--- DataScience/training.py
+++ DataScience/training.py
@@ -7,7 +7,7 @@ from Exceptionhandling import handle
 
 def training():
     try:
-        data = read_csv('data/train.csv')
+        data = read_csv('Data/train.csv')
 
         (data.pipe(missing_values).pipe(texthandling)
              .pipe(categorical_cols_train).pipe(train_and_save_model))
