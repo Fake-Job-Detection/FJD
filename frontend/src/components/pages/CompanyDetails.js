414eb40cc780611529ff9fd48446c2511590905d
ThabittaJudixon <thabitta.2018600@iit.ac.lk>
2021-04-22 19:47:42 +0530

    minor changes in css

--- frontend/src/components/pages/CompanyDetails.js
+++ frontend/src/components/pages/CompanyDetails.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./ComPreview.css";
+import ComVer from "../../ComVer.jpg";
 
 export default function CompanyDetails() {
     return (
-    <div className="preview">
+    <div className="details">
             
     <table>
     <tr>
-    <td colspan = '2'><h3><i>Company Details and Video</i></h3></td> 
+    <td colspan = '2'><h3><i>Company Name and details</i></h3></td> 
+     
+    <div className="logo">
+     <img src={ComVer} height="60" width="60"/>
+     </div>
+    
     <br/>
       <br/>
       </tr>
